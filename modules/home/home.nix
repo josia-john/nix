e38ec13c884c6b4e7c18438b4201f9b0872ab0bf
Josia John <josia.j.john@gmail.com>
2024-05-10 10:33:34 +0200

    fonts, programs

--- modules/home/home.nix
+++ modules/home/home.nix
@@ -2,6 +2,21 @@
 
 {
     home-manager.users.${config.modules.host.username} = {
+        home.username = "${config.modules.host.username}";
+        home.homeDirectory = "/home/${config.modules.host.username}";
+        
+        home.packages = with pkgs; [
+            firefox
+            alacritty
+            htop
+            grim
+            slurp
+            wl-clipboard
+            swaybg
+        ]
+
+        programs.home-manager.enable = true;
+
         home.stateVersion = "23.11";
     };
 }
--- modules/programs.nix
+++ modules/programs.nix
@@ -5,4 +5,8 @@
         vim
         git
     ];
+
+    fonts = {
+        enableDefaultPackages = true;
+    };
 }
