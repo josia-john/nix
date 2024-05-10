b24cf01cda80ef032b6fc3a4ff578a47990f0bfa
Josia John <josia.j.john@gmail.com>
2024-05-10 10:03:16 +0200

    minor sway configs

--- modules/home/sway.nix
+++ modules/home/sway.nix
@@ -2,6 +2,16 @@
 
 {
     home-manager.users.${config.modules.host.username} = {
-        wayland.windowManager.sway.enable = true;
+        wayland.windowManager.sway = {
+            enable = true;
+            config = {
+                modifier = "Mod4";
+                input = {
+                    "keyboard" = {
+                        "xkb_layout" = "dvorak";
+                    };
+                };
+            };
+        };
     };
 }
