0503a4120d41d8310869b1e4468921a79fcdde77
Josia John <josia.j.john@gmail.com>
2024-07-03 13:43:21 +0200

    change config for thiccpad

--- hosts/thiccpad/configuration.nix
+++ hosts/thiccpad/configuration.nix
@@ -1,18 +1,41 @@
-{ config, pkgs, ... }:
-
+{ config, pkgs, lib, ... }:
+with lib;
 {
     imports = [
         ./hardware-configuration.nix
         ./../../modules
     ];
 
-    modules = {
-        host = {
-            hostname = "thiccpad";
-            username = "josia";
+    config = {
+        modules = {
+            host = {
+                hostname = "thiccpad";
+                username = "josia";
+            };
+            home.sway = {
+                wallpaper = ./../../images/X200.png;
+            };
         };
-        home.sway = {
-            wallpaper = ./../../images/X200.png;
+
+        # overwrite boot options:
+
+        boot.loader = lib.mkForce {
+            grub = {
+                enable=true;
+                device="/dev/sda";
+                efiSupport=false;
+                useOSProber=true;
+            };
+        };
+        
+        # overwrite startup:
+
+        
+        environment.etc."scripts/autostart.sh" = lib.mkForce {
+            text = ''
+                
+            '';
         };
     };
+    
 }
